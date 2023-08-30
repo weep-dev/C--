e03a2defdf2b0af5d49c16e6bb42ea4345042220
William Pereira <almeidapereirast@gmail.com>
2023-08-30 18:02:47 -0300

    ponteiros_parte3

--- alocDinMem.cpp
+++ alocDinMem.cpp
@@ -2,3 +2,32 @@
 #include <stdio.h>  //biblioteca do gets
 using namespace std;
 
+void somar(float *var, float valor){
+    *var += valor;
+}
+
+void iniVetor(float *v){
+    for(int i=0; i<5; i++){
+        v[i] = i+1;
+    }
+}
+
+int main(){
+
+    float num = 0;
+    float vet[5];
+
+    iniVetor(vet);
+
+    somar(&num, 15);
+
+    cout << num <<endl;
+
+    for(int i=0; i<5; i++){
+        cout << vet[i] << endl;
+    }
+
+
+
+    return 0;
+}
